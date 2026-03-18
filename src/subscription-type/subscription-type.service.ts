5e8ba2587ae5085435aee0ac0b4896a921e8698c
gtr1701 <dawidkowal04@gmail.com>
2026-03-18 11:35:43 +0100

    feat: basic subscription type endpoints

--- src/subscription-type/subscription-type.service.ts
+++ src/subscription-type/subscription-type.service.ts
@@ -1,18 +1,51 @@
 import { Repository } from "typeorm";
 
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 
+import { SubscriptionTypeDTO } from "./subscription-type.dto";
 import { SubscriptionType } from "./subscription-type.entity";
 
 @Injectable()
 export class SubscriptionTypeService {
   constructor(
     @InjectRepository(SubscriptionType)
-    readonly expenseRepository: Repository<SubscriptionType>,
+    readonly subscriptionRepository: Repository<SubscriptionType>,
   ) {}
 
   async findAll(): Promise<SubscriptionType[]> {
-    return this.expenseRepository.find();
+    return this.subscriptionRepository.find();
+  }
+
+  create(subscriptionTypeBody: SubscriptionTypeDTO[]) {
+    return this.subscriptionRepository.create(subscriptionTypeBody);
+  }
+
+  async update(
+    subscriptionTypeId: number,
+    subscriptionTypeBody: Partial<SubscriptionTypeDTO>,
+  ) {
+    const entryExists = await this.subscriptionRepository.findOne({
+      where: { id: subscriptionTypeId },
+    });
+    if (entryExists == null) {
+      return new NotFoundException();
+    }
+
+    return await this.subscriptionRepository.update(
+      subscriptionTypeId,
+      subscriptionTypeBody,
+    );
+  }
+
+  async delete(subscriptionTypeId: number) {
+    const entryExists = await this.subscriptionRepository.findOne({
+      where: { id: subscriptionTypeId },
+    });
+    if (entryExists == null) {
+      return new NotFoundException();
+    }
+
+    return await this.subscriptionRepository.delete(subscriptionTypeId);
   }
 }
